18a1e740073302d564b0c980e342d60494c404c1
htbhub <42898475+htbhub@users.noreply.github.com>
2022-07-09 15:24:27 -0700

    7.9.22

--- Codewars/7-kyu/main.js
+++ Codewars/7-kyu/main.js
@@ -75,3 +75,4 @@ var isAnagram = function(test, original) {
   return test.toLowerCase().split('').sort().join('') == original.toLowerCase().split('').sort().join('')
 };
 
+
--- Codewars/8-kyu/main.js
+++ Codewars/8-kyu/main.js
@@ -141,3 +141,10 @@ function gooseFilter (birds) {
 function sumStr(a,b) {
   return (+a+ +b)+''  
 }
+
+
+//Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+function nearestSq(n){
+  return Math.round(Math.sqrt(n))**2
+}
