b84dc97db787859d0780c18f73fa67985690480c
htbhub <42898475+htbhub@users.noreply.github.com>
2022-07-09 18:30:14 -0700

    7.9.22

--- Codewars/8-kyu/main.js
+++ Codewars/8-kyu/main.js
@@ -148,3 +148,17 @@ function sumStr(a,b) {
 function nearestSq(n){
   return Math.round(Math.sqrt(n))**2
 }
+
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+  let goodCount = x.filter((idea) => idea === 'good').length
+  return goodCount < 1
+      ? 'Fail!'
+      : goodCount < 3
+      ? 'Publish!'
+      : 'I smell a series!';
+}
